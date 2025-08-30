bbb48fb2bdfe1a53e3a27fed654cdb8cf1b025b7
Enoque Jr <130513321+enoquejrr@users.noreply.github.com>
2025-08-30 21:15:09 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <h1>Vai dar não, to cansadãoooo...!!!!</h1>
-            <img src="/minha-imagem.jpeg" alt="Descrição da imagem" width="300" />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Vai dar não, to cansadãoooo...!!!!</h1>
+      <img src="/minha-imagem.jpeg" alt="Descrição da imagem" width="300" />
+    </div>
+  );
 }
+
+function teste1(
+  console.log("teste");
+)
 
 export default Home;
